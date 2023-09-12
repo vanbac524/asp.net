4bd4b8f5a71b19643eba739c73cd04c10e5e4992
unknown <vanbac524@gmail.com>
2023-09-12 20:18:48 +0700

    add link

---
+++ asp_NguyenVanBac/Controllers/AdmissionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_NguyenVanBac.Controllers
+{
+    public class AdmissionsController : Controller
+    {
+        // GET: Admissions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
