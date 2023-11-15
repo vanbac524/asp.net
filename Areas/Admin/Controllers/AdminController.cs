c1f81e4e7a1682083ea73a5f20b047f5dfab8d70
unknown <vanbac524@gmail.com>
2023-11-15 18:19:33 +0700

    initial commit

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_NguyenVanBac.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,21 @@
+﻿using asp_NguyenVanBac.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_NguyenVanBac.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        websiteBHEntities objwebsiteBHEntities = new websiteBHEntities();
+        public ActionResult Index()
+        {
+            var listProduct = objwebsiteBHEntities.Products.ToList();
+
+            return View(listProduct);
+        }
+    }
+}
